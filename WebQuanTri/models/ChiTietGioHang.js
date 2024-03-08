55f2466d51401f0a7e8979137371479d0b8c6975
Nguyet Ha <nguyetha524@gmail.com>
2024-03-08 18:31:34 +0700

    sửa API chi tiết giỏ hàng và chi tiết hoá đơn

--- WebQuanTri/models/ChiTietGioHang.js
+++ WebQuanTri/models/ChiTietGioHang.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const ChiTietGioHangSchema = mongoose.Schema({
-    soLuong:{type: String},
-    giaTien:{type: String},
+    soLuong:{type: Number},
+    giaTien:{type: Number},
     maChiTietDienThoai:{type: mongoose.Schema.Types.ObjectId, ref: 'chitietdienthoai'},
     maGioHang:{type: mongoose.Schema.Types.ObjectId, ref: 'gioHang'}
     
