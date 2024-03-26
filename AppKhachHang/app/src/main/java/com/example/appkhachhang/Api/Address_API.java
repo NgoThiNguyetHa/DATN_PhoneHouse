4ce2010ca6bc39e8d368db5a4f459dd16741cd69
PV LONG <longpvph29076@fpt.edu.vn>
2024-03-26 08:59:59 +0700

    Sửa api

--- AppKhachHang/app/src/main/java/com/example/appkhachhang/Api/Address_API.java
+++ AppKhachHang/app/src/main/java/com/example/appkhachhang/Api/Address_API.java
@@ -7,6 +7,8 @@ import com.google.gson.GsonBuilder;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
